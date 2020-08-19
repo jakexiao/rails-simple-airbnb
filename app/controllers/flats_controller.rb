4259367b925e068f794ed91f1cbbe9b6e208dcb8
Jake Xiao <xiaosifan0223@hotmail.com>
2020-08-19 18:46:33 +0100

    Add search bar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,11 @@ class FlatsController < ApplicationController
     before_action :set_flat, only: [:show, :edit, :update, :destroy]
   
   def index
-    @flats = Flat.all
+    if params[:query]
+        @flats = Flat.where("name LIKE '%#{params[:query]}%'")
+    else
+        @flats = Flat.all
+    end
   end 
 
   def show 
